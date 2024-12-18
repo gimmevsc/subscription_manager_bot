2454193ae202d8c327e368628572741a298b28a8
BohdanProkopchuk <116494641+gimmevsc@users.noreply.github.com>
2024-12-18 18:37:14 +0100

    Create plans.py

---
+++ util/plans.py
@@ -0,0 +1,22 @@
+subscription_plans = {
+    '1 Day - 1€': {
+        'stripe_price_id': 'price_id',
+        'duration': 1,
+        'plan_name': '1 Day'
+    },
+    '1 Month - 5€': {
+        'stripe_price_id': 'price_id',
+        'duration': 30,
+        'plan_name': '1 Month'
+    },
+    '3 Months - 14€': {
+        'stripe_price_id': 'price_id',
+        'duration': 90,
+        'plan_name': '3 Months'
+    },
+    '1 Year - 49€': {
+        'stripe_price_id': 'price_id',
+        'duration': 365,
+        'plan_name': '1 Year'
+    }
+}
